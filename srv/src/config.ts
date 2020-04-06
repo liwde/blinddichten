589fd98ac8af4aa32124845fad4102e1744e1e2a
Lukas Heimann <mail@liwde.de>
2020-04-06 20:47:11 +0200

    We have a client -- decrease server heartbeat timeouts

--- srv/src/config.ts
+++ srv/src/config.ts
@@ -1,7 +1,7 @@
 export const UI_PATH = '../ui/public';
 export const SERVER_PORT = 5001;
-export const HEARTBEAT_TIMEOUT = 60 * 60 * 1000; // As long as we don't have an app sending a heartbeat, we should be generous
-export const HEARTBEAT_INTERVAL = 60 * 1000;
+export const HEARTBEAT_TIMEOUT = 9 * 1000;
+export const HEARTBEAT_INTERVAL = 3 * 1000;
 export const CLOSE_EMPTY_GAME_TIMEOUT = 10 * 60 * 1000;
 export const DEFAULT_NUM_ROUNDS = 2;
 export const DEFAULT_PLAYER_NAME = 'Unbenannter Spieler';
