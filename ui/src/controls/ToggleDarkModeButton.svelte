f2eaaebf41574044c1e57a801a7777f0b4703740
Lukas Heimann <mail@liwde.de>
2020-04-29 18:35:45 +0200

    Code formatting + remove <button>

--- ui/src/controls/ToggleDarkModeButton.svelte
+++ ui/src/controls/ToggleDarkModeButton.svelte
@@ -1,29 +1,37 @@
-<svelte:head>
-{#if stored === "dark"}
-<link rel='stylesheet' href='darkmode.css' id="dark-mode-css">
-{/if}
-</svelte:head>
-
 <script>
-let stored = localStorage.getItem("dark-mode-storage");
+  let stored = localStorage.getItem("dark-mode-storage");
 
-if (!stored) {
-  if (window.matchMedia("(prefers-color-scheme: dark)").matches ) {
-    stored = "dark";
-  } else {
-    stored = "light";
-  }
-}
-
-function switchTheme() {
-    if (stored === "light") {
-        stored = "dark";
+  if (!stored) {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches ) {
+      stored = "dark";
     } else {
-        stored = "light";
-      }
-}
+      stored = "light";
+    }
+  }
+
+  function switchTheme() {
+    if (stored === "light") {
+      stored = "dark";
+    } else {
+      stored = "light";
+    }
+  }
 </script>
 
-<button on:click="{switchTheme}">
-<img src="img/icon_darkmode.svg" alt="dark mode toggle" class="no-responsive no-border"/>
-</button>
+<svelte:head>
+  {#if stored === "dark"}
+    <link rel='stylesheet' href='darkmode.css' id="dark-mode-css">
+  {/if}
+</svelte:head>
+
+<img src="img/icon_darkmode.svg" alt="dark mode toggle" class="no-responsive no-border" on:click="{switchTheme}"/>
+
+<style>
+  img:hover {
+    opacity: 0.5;
+  }
+  img {
+    height: 30px;
+    vertical-align: middle;
+  }
+</style>
